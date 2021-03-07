fe98cdb9cbdac8019dc2bf317f4056b776965272
lyertia <destek.cubereal@gmail.com>
2021-03-07 18:12:58 +0300

    Hata düzenlemeri

--- src/Events/Ready.js
+++ src/Events/Ready.js
@@ -13,14 +13,16 @@ class Ready extends Base {
     }
     async Execute() {
         console.log(`${Client.user.tag} başarıyla giriş yaptı!`);
-        mcpeping(config.Server.ip, config.Server.port, function(err, res) {
-            if (err) {
-                console.log("Sunucudan bilgi alınamadı.");
-                console.log(err);
-            } else {
-          Client.user.setPresence({ activity: { name: `${res.currentPlayers} kişi ${config.Server.name}` }, status: 'dnd' });
-                    }
-        });
+        setInterval(() => {
+            mcpeping(config.Server.ip, config.Server.port, function(err, res) {
+                        if (err) {
+                            console.log("Sunucudan bilgi alınamadı.");
+                            console.log(err);
+                        } else {
+                      Client.user.setPresence({ activity: { name: `${res.currentPlayers} kişi ${config.Server.name}` }, status: 'dnd' });
+                                }
+                    });
+            },config.Bot.statusDelay*1000)
     }
 }
 
